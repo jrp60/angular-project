c1566a86a057f16e9e63f1adfb29648e126dbf5b
Javier <javirodenas.javirodenas@gmail.com>
2021-05-17 19:58:54 +0200

    maps working
    needs some css

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -3,6 +3,7 @@ import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { MapasService } from "../../services/mapas.service";
 import { FormsModule } from '@angular/forms';
+import { Marcador } from 'src/app/interfaces/marcador.interface';
 
 @Component({
   selector: 'app-maps',
@@ -13,10 +14,56 @@ import { FormsModule } from '@angular/forms';
  
 export class MapsComponent {
 
-  constructor() { }
+  constructor(public _ms:MapasService) {
+    this._ms.cargarMarcadores();
+   }
 
   lat:number = 38.48420283763195;
   lng:number = -0.7677831619109627;
   zoom:number = 15;
+
+  marcadorSel:Marcador = null;
+  draggable:number = 1;
+
+  clickMapa(evento){
+    console.log(evento);
+    console.log("hola");
+    
+    let nuevoMarcador:Marcador = {
+      lat: evento.coords.lat,
+      lng: evento.coords.lng,
+      titulo: "sin titulo",
+      draggable: true
+    }
+    this._ms.insertarMarcador(nuevoMarcador);
+  }
+
+  clickMarcador(marcador:Marcador, i:number){
+    this.marcadorSel = marcador;
+    this.draggable
+    if(this.marcadorSel.draggable){
+      this.draggable = 1;
+    }else{
+      this.draggable = 0;
+    }
+  }
+
+  dragEndMarcador(marcador:Marcador, event){
+    let lat = event.coords.lat;
+    let lng = event.coords.lng;
+
+    marcador.lat = lat;
+    marcador.lng = lng;
+
+    this._ms.guardarMarcadores();
+  }
+
+  cambiarDraggable(){
+    if(this.draggable==1){
+      this.draggable = 0;
+    }else{
+      this.draggable = 1;
+    }
+  }
 
 }
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from "../interfaces/marcador.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapasService {
 
-  constructor() { }
+  marcadores:Marcador[] = [];
+  constructor(){
+    let nuevoMarcador:Marcador = {
+      lat:38.484202837,
+      lng:-0.7677831,
+      titulo: "Explanada de Petrer",
+      draggable: true
+    }
+
+    this.marcadores.push(nuevoMarcador);
+  }
+
+  insertarMarcador(marcador:Marcador){
+    this.marcadores.push(marcador);
+    this.guardarMarcadores();
+  }
+
+  borrarMarcador(i:number){
+    this.marcadores.splice(i,1);
+    this.guardarMarcadores();
+  }
+
+  guardarMarcadores(){
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
+  }
+  cargarMarcadores(){
+    if(localStorage.getItem('marcadores')){
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+    }
+    else{
+      this.marcadores = [];
+    }
+  }
 }
