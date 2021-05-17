c8782a69c3772d626265e44489f40cde27f69222
Javier <javirodenas.javirodenas@gmail.com>
2021-05-17 14:45:51 +0200

    maps in app.module
    not in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-/* import { AutenticadoComponent } from './components/autenticado/autenticado.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { GestorArchivosComponent } from './components/gestor-archivos/gestor-archivos.component';
-import { HomeComponent } from './components/home/home.component';
-import { MapsComponent } from './components/maps/maps.component';
-import { SpotiComponent } from './components/spoti/spoti.component';
-import { YoutubeComponent } from './components/youtube/youtube.component'; */
 
 import { AuthGuard } from '@auth0/auth0-angular';
-/* import { SearchComponent } from './components/spoti/search/search.component';
-import { ArtistComponent } from './components/spoti/artist/artist.component'; */
 
 /* import { AppRoutingModule } from './app-routing.module'; */
 import { HomeComponent } from './components/home/home.component';
@@ -37,12 +27,7 @@ export const APP_ROUTES: Routes = [
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-/* @NgModule({
-  imports: [RouterModule.forRoot(APP_ROUTES)],
-  exports: [RouterModule]
-}) */
-
-@NgModule({ 
+/* @NgModule({ 
   declarations: [
     MapsComponent,
   ],
@@ -54,6 +39,11 @@ export const APP_ROUTES: Routes = [
     ],
   exports: [RouterModule],
   schemas:  [ CUSTOM_ELEMENTS_SCHEMA ]
+}) */
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule {
