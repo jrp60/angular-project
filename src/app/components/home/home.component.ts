da2745768e74571e4c42ea9967d99c8635abf24f
Javier <javirodenas.javirodenas@gmail.com>
2021-08-11 21:18:28 +0200

    download link
    not download, just link

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import 'firebase/storage';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cvImageUrl:string;
+  cvURL:string;
+
+  constructor() {
+    let fbStorage = firebase.storage();
+    fbStorage.ref('CV/jrp-cv.jpg').getDownloadURL().then((downloadURL)=> {
+      this.cvImageUrl = downloadURL;
+    });
+    fbStorage.ref('CV/jrpCV.pdf').getDownloadURL().then((downloadURL)=> {
+      this.cvURL = downloadURL;
+    });
+
+    fbStorage.ref('CV/jrpCV.pdf').getDownloadURL().then(function(url) {
+
+      console.log("THIS LINE NEVER GETS PRINTED");
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = function(event) {
+          Blob = xhr.response;  
+      };
+      xhr.open('GET', url);
+      xhr.send();
+
+      }).then(function() {
+          //some more code
+      }).catch(function(error) {
+          console.log(error);
+      });
+
+  }
 
   ngOnInit() {
   }
